7f5c0023bc84b52ed6563a89e93991e71f5fb45d
ogendogen <kosmos500@interia.pl>
2020-01-10 16:15:18 +0100

    SiteWatcher preparation

--- AdvSteamWatcher/AdvWatcher/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AdvWatcher
-{
-    public class Class1
-    {
-    }
-}
---
+++ AdvSteamWatcher/AdvWatcher/SiteWatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AdvWatcher
+{
+    public class SiteWatcher
+    {
+        public string Site { get; set; }
+        public uint Interval { get; set; }
+        public bool IsWorking { get; set; } = false;
+        public SiteWatcher(string site, uint interval)
+        {
+            Site = site;
+            Interval = interval;
+        }
+
+        public void StartWatcher()
+        {
+            IsWorking = true;
+        }
+
+        public void StopWatcher()
+        {
+            IsWorking = false;
+        }
+    }
+}
