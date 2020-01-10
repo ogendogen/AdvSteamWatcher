62645a4a5a549bb7970f3f8597c22bfe7676bd5e
ogendogen <kosmos500@interia.pl>
2020-01-10 14:45:37 +0100

    NUnit test

--- AdvSteamWatcher/Tests/UnitTest1.cs
+++ AdvSteamWatcher/Tests/UnitTest1.cs
@@ -10,9 +10,21 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        [TestCase(2, 2, ExpectedResult = 4)]
+        [TestCase(3, 3, ExpectedResult = 6)]
+        [TestCase(4, 4, ExpectedResult = 8)]
+        public int SamplePassSumTest(int a, int b)
         {
-            Assert.Pass();
+            return a + b;
+        }
+
+        [Test]
+        [TestCase(3, 2, ExpectedResult = 4)]
+        [TestCase(3, 2, ExpectedResult = 6)]
+        [TestCase(3, 2, ExpectedResult = 8)]
+        public int SampleFailSumTest(int a, int b)
+        {
+            return a + b;
         }
     }
 }
