622a11646e63a1f6238ed73bb3280b10b3769ce1
ogendogen <kosmos500@interia.pl>
2020-01-12 20:15:32 +0100

    SiteParser with wanted text parameter

--- AdvSteamWatcher/AdvWatcher/SiteParser.cs
+++ AdvSteamWatcher/AdvWatcher/SiteParser.cs
@@ -8,16 +8,18 @@ namespace AdvWatcher
     internal class SiteParser
     {
         private string _site;
+        private string _wantedText;
         private WebClient _webClient;
-        public SiteParser(string site)
+        public SiteParser(string site, string wantedText)
         {
             _site = site;
+            _wantedText = wantedText;
         }
 
         public bool IsAdvertismentAvaiable()
         {
             string siteCode = _webClient.DownloadString(_site);
-            return !siteCode.Contains("Limit reached, week boost will be available on ");
+            return !siteCode.Contains(_wantedText);
         }
     }
 }
