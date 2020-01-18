5df8ea5dde931a486b15cd65e89289b4618a4f6b
ogendogen <kosmos500@interia.pl>
2020-01-18 13:45:25 +0100

    Done full CloudflareBanger script

--- AdvSteamWatcher/CloudflareBanger/CloudflareBanger.py
+++ AdvSteamWatcher/CloudflareBanger/CloudflareBanger.py
@@ -1,6 +1,16 @@
-
 import cloudscraper
-scraper = cloudscraper.create_scraper()
-print("start")
-print(scraper.get("https://gametracker.rs").content)
-print("done")
+import sys
+
+args = sys.argv
+args_len = len(sys.argv)
+args_len = args_len - 1
+if args_len != 1:
+    exit("[ERROR] Incorrect arguments amount (expected 1, got %d)" % args_len)
+
+targetSite = args[1]
+
+try:
+    scraper = cloudscraper.create_scraper()
+    exit(scraper.get(targetSite).content)
+except Exception as e:
+    exit("[ERROR] %s" % str(e))
