7f03e1a28104d3a2b83c8102e2dcdfa697c56c9f
ogendogen <kosmos500@interia.pl>
2020-01-18 14:38:29 +0100

    Fixed CFBanger exit problem

--- AdvSteamWatcher/CloudflareBanger/CloudflareBanger.py
+++ AdvSteamWatcher/CloudflareBanger/CloudflareBanger.py
@@ -5,12 +5,13 @@ args = sys.argv
 args_len = len(sys.argv)
 args_len = args_len - 1
 if args_len != 1:
-    exit("[ERROR] Incorrect arguments amount (expected 1, got %d)" % args_len)
+    print("[ERROR] Incorrect arguments amount (expected 1, got %d)" % args_len)
+    exit(0)
 
 targetSite = args[1]
 
 try:
     scraper = cloudscraper.create_scraper()
-    exit(scraper.get(targetSite).content)
+    print(scraper.get(targetSite).content)
 except Exception as e:
-    exit("[ERROR] %s" % str(e))
+    print("[ERROR] %s" % str(e))
