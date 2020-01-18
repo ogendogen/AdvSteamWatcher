6df41f518d6293309a1bbe0d05f981fdcf092e33
ogendogen <kosmos500@interia.pl>
2020-01-19 00:59:02 +0100

    Added python script timer calling test

--- AdvSteamWatcher/Tests/SiteWatcherTests.cs
+++ AdvSteamWatcher/Tests/SiteWatcherTests.cs
@@ -1,13 +1,37 @@
 using NUnit.Framework;
+using AdvWatcher;
+using System.Threading;
+using System;
 
 namespace Tests
 {
     public class SiteWatcherTests
     {
+        private bool isEventCalled;
         [SetUp]
         public void Setup()
         {
+            isEventCalled = false;
+        }
 
+        [Test]
+        [TestCase("https://www.gametracker.rs/sms_boost/", 15.00, "PayPal boost")]
+        public void GameTrackerPayPalBoostTest(string site, double interval, string wantedText)
+        {
+            SiteWatcher siteWatcher = new SiteWatcher(site, interval, wantedText);
+            siteWatcher.OnAdvAvaiable += SiteWatcher_OnAdvAvaiable;
+            siteWatcher.StartWatcher();
+
+            int steps = (Convert.ToInt32(interval) * 1000) * 4;
+            Thread.Sleep(steps);
+
+            siteWatcher.StopWatcher();
+            Assert.IsTrue(isEventCalled, "Watcher stopped and string not found");
+        }
+
+        private void SiteWatcher_OnAdvAvaiable(object sender, System.EventArgs e)
+        {
+            isEventCalled = true;
         }
     }
 }
