db7603f017ac9f921270bef5e4035beb0af4c088
ogendogen <kosmos500@interia.pl>
2020-01-25 21:45:17 +0100

    Filled Config class with properties

--- AdvSteamWatcher/SteamBot/Config.cs
+++ AdvSteamWatcher/SteamBot/Config.cs
@@ -6,6 +6,8 @@ namespace Steam
 {
     public class Config
     {
-
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public List<string> AdvMessageReceivers { get; set; }
     }
 }
