0a9e90e5390e0b557c6d6d75491d45ed495d7110
ogendogen <kosmos500@interia.pl>
2020-01-25 22:00:11 +0100

    Initialized list in Config's constructor

--- AdvSteamWatcher/SteamBot/Config.cs
+++ AdvSteamWatcher/SteamBot/Config.cs
@@ -9,5 +9,10 @@ namespace Steam
         public string Login { get; set; }
         public string Password { get; set; }
         public List<string> AdvMessageReceivers { get; set; }
+
+        public Config()
+        {
+            AdvMessageReceivers = new List<string>();
+        }
     }
 }
