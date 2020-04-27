cab8f2aba7aa5790dd921208190723d5627e29e3
ogendogen <kosmos500@interia.pl>
2020-04-27 20:28:00 +0200

    Fixed RPI version

--- AdvSteamWatcher/AdvWatcher/SiteParser.cs
+++ AdvSteamWatcher/AdvWatcher/SiteParser.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Net;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace AdvWatcher
 {
@@ -19,12 +20,23 @@ namespace AdvWatcher
             _wantedText = wantedText;
             _pathToScript = pathToScript;
 
+            string fileName = String.Empty;
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                fileName = "/usr/local/bin/python3.8";
+            }
+            else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                fileName = "py";
+            }
+
             _pythonProcess = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = "py",
-                    Arguments = $"{_pathToScript} { _site }",
+                    FileName = fileName,
+                    ArgumentList = {_pathToScript, _site},
+                    //Arguments = $"{_pathToScript} { _site }",
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
                     CreateNoWindow = true
@@ -34,11 +46,12 @@ namespace AdvWatcher
 
         public bool IsAdvertismentAvaiable()
         {
-            _pythonProcess.Start();
+            string pythonOutput = String.Empty;
 
             try
             {
-                string pythonOutput = _pythonProcess.StandardOutput.ReadToEnd();
+                _pythonProcess.Start();
+                pythonOutput = _pythonProcess.StandardOutput.ReadToEnd();
                 if (pythonOutput.Contains("[ERROR]"))
                 {
                     File.AppendAllText($"errors{ DateTime.Now.ToString("yyyyMMdd") }.log", DateTime.Now.ToString() + " " + pythonOutput);
@@ -50,6 +63,11 @@ namespace AdvWatcher
             {
                 File.AppendAllText($"errors{ DateTime.Now.ToString("yyyyMMdd") }.log", DateTime.Now.ToString() + " " + e.Message);
                 return false;
+            }
+            finally
+            {
+                File.WriteAllText("py_output.log", pythonOutput);
+                _pythonProcess.Close();
             }
         }
     }
