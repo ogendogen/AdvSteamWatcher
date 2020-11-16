77f0a8eeae9025ba6e38b96e344eaf6f10d2a161
ogendogen <kosmos500@interia.pl>
2020-11-16 23:26:03 +0100

    Added antirecaptcha token

--- AdvSteamWatcher/CloudflareBanger/CloudflareBanger.py
+++ AdvSteamWatcher/CloudflareBanger/CloudflareBanger.py
@@ -11,7 +11,13 @@ if args_len != 1:
 targetSite = args[1]
 
 try:
-    scraper = cloudscraper.create_scraper()
+    scraper = cloudscraper.create_scraper(
+        interpreter='nodejs',
+        recaptcha={
+            'provider': 'anticaptcha',
+            'api_key': '7d85e1a586181939858d2cbc41b087d3'
+        }
+    )
     print(scraper.get(targetSite).content)
 except Exception as e:
     print("[ERROR] %s" % str(e))
